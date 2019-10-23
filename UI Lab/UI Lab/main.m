b1836045c46f8c570112c1542e84a1834f0b3fa8
nakulnandhakumar <55005060+nakulnandhakumar@users.noreply.github.com>
2019-10-24 06:47:06 +0900

    Add files via upload

---
+++ UI Lab/UI Lab/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UI Lab
+//
+//  Created by Nandhakumar, Nakul on 10/23/19.
+//  Copyright © 2019 Nandhakumar, Nakul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
